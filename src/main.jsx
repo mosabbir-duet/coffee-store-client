cf6d5b8a1ec1ed9451e1f69e59cc21b41c6f904c
mosabbir.duet <mosabbir.duet@gmail.com>
2023-05-08 11:48:35 +0600

    Project Setup

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import AddCoffee from "./components/AddCoffee";
+import UpdateCoffee from "./components/UpdateCoffee";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/addCoffee",
+    element: <AddCoffee></AddCoffee>,
+  },
+  {
+    path: "/updateCoffee",
+    element: <UpdateCoffee></UpdateCoffee>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
